d22f4fd751e146ac83a456add80c63050812718c
dilyana2001 <dilyana2001@gmail.com>
2021-05-31 14:21:34 +0300

    upload dictionary solution

--- objects-n-classes.js
+++ objects-n-classes.js
@@ -59,3 +59,38 @@ class Laptop {
 
 
 // =================================================================================================================================================
+
+
+// You will receive an array with strings in the form of JSON's.
+// You have to parse these strings and combine them into one object. Every string from the array will hold terms and a
+// description. If you receive the same term twice replace it with the new definition.
+// Print every term and definition in that dictionary on new line in format:
+// Term: ${term} => Definition: ${definition}
+// Don't forget to sort the dictionary alphabetically by the terms as in real dictionaries.
+
+function dictionary(json) {
+    let dictionary = {}
+    for (let line of json) {
+        let parsed = JSON.parse(line)
+        for (let key in parsed) {
+            dictionary[key] = parsed[key]
+        }
+    }
+    let sortedDictionary = Object.entries(dictionary).sort((a, b) => a[0].localeCompare(b[0]))
+    for (let key of sortedDictionary) {
+        console.log(`Term: ${key[0]} => Definition: ${key[1]}`)
+    }
+}
+// // expected output:
+// // Term: Boiler => Definition: A fuel-burning apparatus or container for heating water.
+// // Term: Bus => Definition: A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare.
+// // Term: Coffee => Definition: A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub.
+// // Term: Microphone => Definition: An instrument for converting sound waves into electrical energy variations  which may then be amplified, transmitted, or recorded.
+// // Term: Tape => Definition: A narrow strip of material, typically used to hold  or fasten something.
+// dictionary([
+//     '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+//     '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+//     '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+//     '{"Tape":"A narrow strip of material, typically used to hold  or fasten something."}',
+//     '{"Microphone":"An instrument for converting sound waves into electrical energy variations  which may then be amplified, transmitted, or recorded."}'
+// ])
