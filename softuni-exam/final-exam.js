42dc433a65f3537ce8346f565de143e53e9bf12c
dilyana2001 <dilyana2001@gmail.com>
2021-06-18 21:52:29 +0300

    updates

--- softuni-exam/final-exam.js
+++ softuni-exam/final-exam.js
@@ -97,15 +97,15 @@ function heroesOfCodeAndLogicVII(arr) {
 }
 
 // heroesOfCodeAndLogicVII([
-//     '2',
-//     'Solmyr 85 120',
-//     'Kyrre 99 50',
-//     'Heal - Solmyr - 10',
-//     'Recharge - Solmyr - 50',
-//     'TakeDamage - Kyrre - 66 - Orc',
-//     'CastSpell - Kyrre - 15 - ViewEarth',
-//     'End'
-// ])
+//         '2',
+//         'Solmyr 85 120',
+//         'Kyrre 99 50',
+//         'Heal - Solmyr - 10',
+//         'Recharge - Solmyr - 50',
+//         'TakeDamage - Kyrre - 66 - Orc',
+//         'CastSpell - Kyrre - 15 - ViewEarth',
+//         'End'
+//     ])
 // heroesOfCodeAndLogicVII([
 //     '4',
 //     'Adela 90 150',
