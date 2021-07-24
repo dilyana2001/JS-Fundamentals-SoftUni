b7f8531821f9cbb5faa0530c6d5f94b688c03ea3
dilyana2001 <dilyana2001@gmail.com>
2021-07-24 10:26:17 +0300

    edits

--- objects/Associative-arrays/arena-tier.js
+++ objects/Associative-arrays/arena-tier.js
@@ -1,84 +1,73 @@
 function solve(arr) {
     let heroes = {};
-    let result = 0;
-    // Gladius: 700 skill
-    // - Shield <!> 250
-    // - Support <!> 250
-    // - Heal <!> 200
-    // Peter: 400 skill
-    // - Duck <!> 400
     arr.forEach(line => {
-                let [heroName, skill, points] = line.split(' -> ');
-                points = Number(points);
-                if (heroName != 'Ave Cesar') {
-                    if (points) {
-                        if (!heroes.hasOwnProperty(heroName)) {
-                            result = points
-                            heroes[heroName] = [{ skill, points, result }];
-                        } else {
-                            result += points
-                            heroes[heroName].push({ skill, points, result });
-                        }
-                    } else {
-                        let [firstHero, secondHero] = heroName.split(' vs ')
-                        if (heroes.hasOwnProperty(firstHero) && heroes.hasOwnProperty(secondHero)) {
-                            let g1 = Object.entries(heroes[firstHero]);
-                            let g2 = Object.entries(heroes[secondHero]);
-                            let isBattle = false;
-                            g1.forEach(el1 => {
-                                g2.forEach(el2 => {
-                                    if (el1[0] === el2[0]) {
-                                        isBattle = true;
-                                    }
-                                });
-                            });
-                            if (isBattle) {
-                                console.log(heroes[firstHero])
+        let [heroName, skill, points] = line.split(' -> ');
+        points = Number(points);
+        if (heroName != 'Ave Cesar') {
+            if (points) {
+                if (!heroes.hasOwnProperty(heroName)) {
+                    heroes[heroName] = [{ skill, points }];
+                } else {
+                    let found = false;
+                    for (let update in heroes[heroName]) {
+                        if (skill === heroes[heroName][update].skill) {
+                            found = true;
+                            if (points > heroes[heroName][update].points) {
+                                heroes[heroName][update].points = points;
                             }
+                            break;
                         }
                     }
-                } else {
-                    // Slowest but very readable and doesn't require a variable way for finding the last or the only one element of an array
-                    // myArray.slice(-1)[0]
-                    Object.entries(heroes).sort((a, b) => b[1].slice(-1)[0].result - a[1].slice(-1)[0].result)
-                        .map(x => console.log(`${x[0]}: ${x[1].slice(-1)[0].result} skill\n${x[1]
-                            .sort((a,b)=>a.skill.localeCompare(b.skill)).sort((a, b) => b.points - a.points)
-                            .map(x=>`- ${x.skill} <!> ${x.points}`).join('\n')}`));
-                         return;
+                    if (!found) {
+                        heroes[heroName].push({ skill, points });
+                    }
+                }
+            } else {
+                let [firstHero, secondHero] = heroName.split(' vs ')
+                if (heroes.hasOwnProperty(firstHero) && heroes.hasOwnProperty(secondHero)) {
+                    Object.values(heroes[firstHero]).forEach(el1 => {
+                        if (heroes.hasOwnProperty(firstHero) && heroes.hasOwnProperty(secondHero)) {
+                            Object.values(heroes[secondHero]).forEach(el2 => {
+                                if (el1.skill === el2.skill) {
+                                    let totalFirstHero = Object.values(heroes[firstHero]).reduce((a, b) => a + b.points, 0);
+                                    let totalSecondHero = Object.values(heroes[secondHero]).reduce((a, b) => a + b.points, 0);
+                                    if (totalFirstHero > totalSecondHero) {
+                                        delete heroes[secondHero];
+                                    } else {
+                                        delete heroes[firstHero];
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            }
         }
     });
+
+    Object.entries(heroes).sort((a, b) => (b[1].reduce((a, b) => a + b.points, 0) - a[1].reduce((a, b) => a + b.points, 0)) || a[0].localeCompare(b[0]))
+        .map(x => console.log(`${x[0]}: ${x[1].reduce((a, b) => a + b.points, 0)} skill\n${x[1]
+            .sort((a, b) => b.points - a.points || a.skill.localeCompare(b.skill))
+            .map(x => `- ${x.skill} <!> ${x.points}`).join('\n')}`));
 }
 
-solve([
-    'Peter -> Duck -> 400',
-    'Julius -> Shield -> 150',
-    'Gladius -> Heal -> 200',
-    'Gladius -> Support -> 250',
-    'Gladius -> Shield -> 250',
-    'Peter vs Gladius',
-    // Gladius and Peter don't have
-// common technique, so the duel
-// isn't valid.
-    'Gladius vs Julius',
-    // Gladius wins vs Julius /common
-// technique: 'Shield'. Julius is
-// demoted.
-    'Gladius vs Maximilian',
-    // Maximilian doesn't exist so the
-// duel isn't valid.
-    'Ave Cesar'
-    ])
-    // Stefan: 450 skill
-// - Tiger <!> 250
-// - Duck <!> 200
+// Gladius: 700 skill
+// - Shield <!> 250
+// - Support <!> 250
+// - Heal <!> 200
 // Peter: 400 skill
-// - BattleCry <!> 400
-// Alex: 300 skill
-// - PowerPunch <!> 300
-// solve([
-//         'Peter -> BattleCry -> 400',
-//         'Alex -> PowerPunch -> 300',
-//         'Stefan -> Duck -> 200',
-//         'Stefan -> Tiger -> 250',
-//         'Ave Cesar'
-//     ])
+// - Duck <!> 400
+solve([
+        'Peter -> Duck -> 400',
+        'Peter -> Duck -> 800',
+        'Julius -> Shield -> 150',
+        'Gladius -> Heal -> 200',
+        'Gladius -> Support -> 250',
+        'Gladius -> Support -> 350',
+        'Gladius -> Shield -> 250',
+        'Gladius -> Duck -> 800',
+        'Peter vs Gladius',
+        'Gladius vs Julius',
+        'Gladius vs Maximilian',
+        'Ave Cesar'
+    ])
