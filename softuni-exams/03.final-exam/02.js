cd4d560a602621703a63d5e6e764156954c5824a
dilyana2001 <dilyana2001@gmail.com>
2021-08-12 20:04:12 +0300

    ad astra solution

--- softuni-exams/03.final-exam/02.js
+++ softuni-exams/03.final-exam/02.js
@@ -1,25 +1,25 @@
 function mirrorWords(arr) {
-    let input = arr.join('')
-    let regex = /(#|@)([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1/g
-    let exactPairs = []
-    let match = input.match(regex)
+    let regex = /(#|@)([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1/g;
+    let exactPairs = [];
+    let match = arr.join('').match(regex);
+    if (match == null) {
+        match = []
+    }
     for (let i = 0; i < match.length; i++) {
-        let current = match[i].split('')
-        let reversed = match[i].split('').reverse()
+        let current = match[i].split('');
+        let reversed = match[i].split('').reverse();
         if (current.join('') === reversed.join('')) {
-            let mirrorWord = match[i].substring(1, match[i].length / 2 - 1)
+            let mirrorWord = match[i].substring(1, match[i].length / 2 - 1);
             exactPairs.push(`${mirrorWord} <=> ${mirrorWord.split('').reverse().join('')}`)
         }
     }
     if (match.length > 0) {
         console.log(`${match.length} word pairs found!`);
-
     } else {
         console.log(`No word pairs found!`)
     }
     if (exactPairs.length > 0) {
-        console.log(`The mirror words are: `);
-        console.log(`${exactPairs.join(', ')}`);
+        console.log(`The mirror words are: \n${exactPairs.join(', ')}`);
     } else {
         console.log(`No mirror words!`);
     }
